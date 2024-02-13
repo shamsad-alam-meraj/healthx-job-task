d62f05208f8bb63a24acffea9edd4199e1f64f57
Shamsad Alam Meraj <shamsadalammeraj9@outlook.com>
2024-02-13 20:26:32 +0600

    facility complete

--- src/components/pages-components/Landing/SingleFacilites.jsx
+++ src/components/pages-components/Landing/SingleFacilites.jsx
@@ -1,14 +1,15 @@
+import styles from "../../../styles/FacilitesStyle.module.css"
 // eslint-disable-next-line react/prop-types
 const SingleFacilities = ({ imageSrc, title, subtitle }) => {
   console.log(imageSrc);
   return (
-    <div>
-      <div>
-        <img src={imageSrc} alt="image" />
+    <div className={styles["facility-container"]}>
+      <div className={styles['image-container']}>
+        <img className={styles['image-style']} src={imageSrc} alt="image" />
       </div>
-      <div>
-        <h6>{title}</h6>
-        <p>{subtitle}</p>
+      <div className={styles['description-container']}>
+        <p className={styles['title-style']}>{title}</p>
+        <p className={styles['subtitle-style']}>{subtitle}</p>
       </div>
     </div>
   );
