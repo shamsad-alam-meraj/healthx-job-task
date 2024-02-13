cec1ab27dd7fa0bbf203de2774bf56a680d00e52
Shamsad Alam Meraj <shamsadalammeraj9@outlook.com>
2024-02-13 23:45:09 +0600

    design completed

--- src/components/pages-components/Landing/SingleProduct.jsx
+++ src/components/pages-components/Landing/SingleProduct.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/prop-types */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../../../styles/ProductsStyle.module.css";
 import { Rating } from "react-simple-star-rating";
+import { faBagShopping, faRetweet } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 const SingleProduct = ({
   title,
@@ -15,7 +18,31 @@ const SingleProduct = ({
       <div className={styles["image-container"]}>
         <img className={styles["image-style"]} src={productImage} alt="" />
       </div>
-      <div className={styles['show-percentage']}>{"-"}{discountPercentage}%</div>
+      <div className={styles["show-percentage"]}>
+        {"-"}
+        {discountPercentage}%
+      </div>
+      <div className={styles["cart-container"]}>
+        <div className={styles["cart-item"]}>
+          <FontAwesomeIcon
+            icon={faBagShopping}
+            style={{ color: "black", fontSize: "15px" }}
+          />
+        </div>
+        <div className={styles["cart-item"]}>
+          <FontAwesomeIcon
+            icon={faHeart}
+            style={{ color: "black", fontSize: "15px" }}
+          />
+        </div>
+        <div className={styles["cart-item"]}>
+          <FontAwesomeIcon
+            icon={faRetweet}
+            style={{ color: "black", fontSize: "15px" }}
+          />
+        </div>
+        {/* <div></div> */}
+      </div>
       {/* description section  */}
       <div className={styles["description-section"]}>
         <p className={styles["product-title"]}>{title}</p>
@@ -38,7 +65,7 @@ const SingleProduct = ({
           <p className={styles["discounted-price"]}>
             ${(price - (price * discountPercentage) / 100).toFixed(2)}
           </p>
-          <p className={styles['original-price']}>{price}</p>
+          <p className={styles["original-price"]}>{price}</p>
         </div>
       </div>
     </div>
