f3efe21812d4b34a8ca4f57d3a85bcf911045341
Shamsad Alam Meraj <shamsadalammeraj9@outlook.com>
2024-02-15 20:38:58 +0600

    sidebar added

--- src/components/pages-components/Landing/Banner.jsx
+++ src/components/pages-components/Landing/Banner.jsx
@@ -2,18 +2,25 @@ import styles from "../../../styles/BannerStyle.module.css";
 const Banner = () => {
   return (
     <div className={styles["banner-content"]}>
-      <div className={styles['main-details-container']}>
+      <div className={styles["main-details-container"]}>
         <div className={styles["description-container"]}>
           <p className={styles["subheader-style"]}>
-            Up to <span className={styles["percentage-style"]}>70%</span> of on
+            Up to <span className={styles["percentage-style"]}>70%</span> off on
             Black Friday
           </p>
-          <p className={styles["headline-style"]}>Trendy Fashion Collection</p>
+          <p className={styles["headline-style"]}>
+            Trendy <span className={styles["fashion-style"]}>Fashion</span>
+            <br /> Collection
+          </p>
           <button className={styles["btn-style"]}>Buy Now</button>
         </div>
       </div>
       <div className={styles["image-container"]}>
-        <img className={styles['image-style']} src="/assets/images/banner.png" alt="banner" />
+        <img
+          className={styles["image-style"]}
+          src="/assets/images/banner.png"
+          alt="banner"
+        />
       </div>
     </div>
   );
--- src/components/pages-components/Landing/Sidebar.jsx
+++ src/components/pages-components/Landing/Sidebar.jsx
@@ -1,5 +1,101 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "../../../styles/SidebarStyle.module.css";
+import { faAngleRight, faPlus } from "@fortawesome/free-solid-svg-icons";
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div className={styles["sidebar-container"]}>
+      <li className={styles["first-item"]}>
+        <span>Fashion Collection</span>
+        <span>
+          <FontAwesomeIcon
+            icon={faAngleRight}
+            style={{ color: "#007bff", fontSize: "14px" }}
+          />
+        </span>
+      </li>
+      <li className={styles["list-item"]}>
+        Electronic Items
+        <span>
+          <FontAwesomeIcon
+            icon={faAngleRight}
+            style={{ color: "black", fontSize: "14px" }}
+          />
+        </span>
+      </li>
+      <li className={styles["list-item"]}>
+        Home Applicance
+        <span>
+          <FontAwesomeIcon
+            icon={faAngleRight}
+            style={{ color: "black", fontSize: "14px" }}
+          />
+        </span>
+      </li>
+      <li className={styles["list-item"]}>
+        Kitchen Items
+        <span>
+          <FontAwesomeIcon
+            icon={faAngleRight}
+            style={{ color: "black", fontSize: "14px" }}
+          />
+        </span>
+      </li>
+      <li className={styles["list-item"]}>
+        Furniture
+        <span>
+          <FontAwesomeIcon
+            icon={faAngleRight}
+            style={{ color: "black", fontSize: "14px" }}
+          />
+        </span>
+      </li>
+      <li className={styles["list-item"]}>
+        Food
+        <span>
+          <FontAwesomeIcon
+            icon={faAngleRight}
+            style={{ color: "black", fontSize: "14px" }}
+          />
+        </span>
+      </li>
+      <li className={styles["list-item"]}>
+        Gadgets
+        <span>
+          <FontAwesomeIcon
+            icon={faAngleRight}
+            style={{ color: "black", fontSize: "14px" }}
+          />
+        </span>
+      </li>
+      <li className={styles["list-item"]}>
+        Toys and Games
+        <span>
+          <FontAwesomeIcon
+            icon={faAngleRight}
+            style={{ color: "black", fontSize: "14px" }}
+          />
+        </span>
+      </li>
+      <li className={styles["list-item"]}>
+        Health & Beauty
+        <span>
+          <FontAwesomeIcon
+            icon={faAngleRight}
+            style={{ color: "black", fontSize: "14px" }}
+          />
+        </span>
+      </li>
+      <li className={styles["last-item"]}>
+        View All Categories
+        <span>
+          <FontAwesomeIcon
+            icon={faPlus}
+            style={{ color: "#007bff", fontSize: "14px" }}
+          />
+        </span>
+      </li>
+    </div>
+  );
 };
 
 export default Sidebar;
